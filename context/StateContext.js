1d23308cad80f3d5e42455d80da8afaf4a2630bb
Vaibhav Kesharwani <vaibhav.vk2128@gmail.com>
2023-03-11 01:00:02 +0530

    Initial commit

---
+++ context/StateContext.js
@@ -0,0 +1,8 @@
+import { createContext, useContext, useState, useEffect } from "react";
+import { toast } from "react-hot-toast";
+
+const Context = createContext();
+
+export const StateContext = ({ children }) => {
+    
+};
